b57a98c53e21c563b1a72667567e6bba7feaa7d5
mazal <mazalwebdesign@gmail.com>
2021-05-13 17:02:30 +0300

    adding app-use-helmet

--- myApp.js
+++ myApp.js
@@ -4,6 +4,16 @@ var app = express();
 let helmet = require('helmet');
 
 app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({action: 'deny'})); 
+app.use(helmet.xssFilter({}));
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+var ninetyDaysInMilliseconds = 90*24*60*60*1000; 
+app.use(helmet.hsts({ maxAge: ninetyDaysInMilliseconds, force: true }));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] }} ))
+
 
 
 
